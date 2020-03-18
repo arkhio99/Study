cd690d23a3740e8fc0bd0241c4197bbc059e100b
Vlad <61151660+arkhio99@users.noreply.github.com>
2020-03-18 14:06:22 +0500

    added new functionality

--- New_project/Program.cs
+++ New_project/Program.cs
@@ -6,7 +6,7 @@ namespace New_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("added new functionality");
         }
     }
 }
