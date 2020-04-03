57081ceb00c081c5763bfd6710f6b2016b1089f7
Vlad <61151660+arkhio99@users.noreply.github.com>
2020-04-03 21:31:44 +0500

    Check downcast

--- New_project/Program.cs
+++ New_project/Program.cs
@@ -2,11 +2,25 @@
 
 namespace New_project
 {
+    public class A:
+    {
+        int i;
+        public A(int a)
+        {
+            i=a;
+        }
+        public geti()
+        {
+            return i;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Object o= new Object();
+            A a=(A)o;
+            System.Console.WriteLine(a.geti());
         }
     }
 }
