73bb0e331597041ebfea7a99d38362de26808047
Vlad <61151660+arkhio99@users.noreply.github.com>
2020-04-14 12:42:23 +0500

    check extension methods

--- New_project/Program.cs
+++ New_project/Program.cs
@@ -2,56 +2,31 @@
 
 namespace New_project
 {
-    class A{
+    internal class A{
         public Int32 i;
         public A(Int32 _i)
         {
             i=_i;
         }
-        public B ToB()
-        {
-            return new B(i);
-        }
-        public static explicit operator A(B b)
-        {
-            return new A(b.i);
-        }
     }
-    class B
+    
+    internal static class AExtension
     {
-        public Int32 i;
-        internal B(Int32 _i)
+        public static void SayHi(this A a)
         {
-            i=_i;
+            //for(int i=0;i<a.i;i++)
+            System.Console.WriteLine("Hi!");
         }
-        public B(A a)
-        {
-            i=a.i;
-        }
-        public Int32 ToInt32()
-        {
-            return i;
-        }
-        public static implicit operator B(A a)
-        {
-            return new B(a);
-        }
-        /*public static explicit operator Int32(B b)
-        {
-            return b.ToInt32();
-        }*/
     }
+
     class Program
     {
         static void Main(string[] args)
         {
             A a=new A(3);
-            B bFromAImp=a;
-            A aFromB=(A)bFromAImp;
-            B bFromAExp=(B)a;
-            System.Console.WriteLine(bFromAImp.i);
-            System.Console.WriteLine(aFromB.i);
-            System.Console.WriteLine(bFromAExp.i);
+            a.SayHi();
+            A n=null;
+            n.SayHi();
         }
     }
 }
