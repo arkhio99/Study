b858c1a2c304eb85e9461d45bc8ae8a9a2b6148d
Vlad <61151660+arkhio99@users.noreply.github.com>
2020-04-19 19:44:31 +0500

    delegats

--- New_project/Program.cs
+++ New_project/Program.cs
@@ -2,28 +2,47 @@
 
 namespace New_project
 {
-    sealed partial class A
-    {
-        partial void f(); 
-        public Int32 SayHi()
-        {
-            f();
-            return 0;
-        }
-    }
-    sealed partial class A{
-        partial void f()
-        {
-            System.Console.WriteLine("Hi");
-        }
-    }
+    delegate int function(int x);
+
+    
+
+
 
     class Program
     {
+        static int[] ArOfFunction(int[] x, function f)
+    {
+        int[] res=new int[x.Length];
+        for(int i=0;i<x.Length;i++)
+        {
+            res[i]=f(x[i]);
+        }
+        return res;
+    }
         static void Main(string[] args)
         {
-            A a = new A();
-            System.Console.WriteLine(a.SayHi());
+            function f=delegate(int x)
+            {
+                return x*x;
+            };
+            int[] x=new int[10]
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+            int[] sqrs=ArOfFunction(x,f);
+            for(int i=0;i<sqrs.Length;i++)
+            {
+                System.Console.Write(sqrs[i].ToString()+" ");
+            }
+            System.Console.WriteLine("\n");
+            int[] cubes=ArOfFunction(x,delegate(int x) 
+            {
+                return x*x*x;
+            });
+            for(int i=0;i<cubes.Length;i++)
+            {
+                System.Console.Write(cubes[i].ToString()+" ");
+            }
         }
     }
 }
